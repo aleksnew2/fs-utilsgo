8c786fba461e50457d9f188e53dda814080085b3
aleksnew2 <aleks.koyf@gmail.com>
2024-08-15 22:14:00 +0300

    initial commit

---
+++ file.go
@@ -0,0 +1,129 @@
+// Package fs_utils provides function for creating, removing files etc.
+package fs_utils
+
+import (
+	"bufio"
+	"os"
+)
+
+// File is a structure with information about file.
+// File should be initialized by functions
+// CreateFileQ or CreateFileW.
+// Can be removed by RemoveFileW
+type File struct {
+	Path    string
+	Content []string
+}
+
+// emptyFileW makes property f empty.
+func emptyFileW(f *File) {
+	f = nil
+}
+
+// emptyFileQ makes property
+// and returns content of file.
+func emptyFileQ(f *File) []string {
+	lastContent := f.Content
+	f.Path = ""
+	f.Content = nil
+	return lastContent
+}
+
+// GetFile returns path file. Checks their availability.
+// If file doesn't exist, return empty string and error.
+func GetFile(path string) (string, error) {
+	if _, err := os.Stat(path); err != nil {
+		return "", err
+	}
+
+	return path, nil
+}
+
+// CreateFileQ creates file to specific path.
+// If file already exists, then returns error.
+func CreateFileQ(path string) error {
+	if _, err := os.Stat(path); err != nil {
+		return err
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+	return nil
+}
+
+// CreateFileW creates file to specific path,
+// then writes content to file.
+// Every element of content is new line.
+// If file already exists, then returns error.
+func CreateFileW(path string, content ...string) error {
+	if _, err := os.Stat(path); err != nil {
+		return err
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+
+	writer := bufio.NewWriter(file)
+
+	defer func(writer *bufio.Writer) {
+		_ = writer.Flush()
+	}(writer)
+
+	for _, v := range content {
+		if _, err := writer.WriteString(v + "\n"); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// RemoveFileQ removes file from specific path.
+// If it couldn't find, then returns error.
+func RemoveFileQ(path string) error {
+	if _, err := os.Stat(path); err != nil {
+		return err
+	}
+
+	if err := os.Remove(path); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RemoveFileW remove file, but from structure File.
+// If it couldn't find, then returns error.
+func RemoveFileW(f *File) error {
+	if _, err := os.Stat(f.Path); err != nil {
+		return err
+	}
+
+	emptyFileW(f)
+
+	return nil
+}
+
+// RemoveFileA remove file, but from structure File.
+// If it couldn't find, then returns error.
+func RemoveFileA(f *File) ([]string, error) {
+	if _, err := os.Stat(f.Path); err != nil {
+		return nil, err
+	}
+
+	content := emptyFileQ(f)
+
+	return content, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aleksnew2/fs-utils
+
+go 1.22
