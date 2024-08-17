d51808f19e38386d6c9999639464f72bd5b42cbf
aleksnew2 <aleks.koyf@gmail.com>
2024-08-17 13:06:12 +0300

    dir.go: create ReadDir

--- dir.go
+++ dir.go
@@ -1,8 +1,34 @@
 package fs_utils
 
 import (
+	"io/fs"
 	"os"
+	"path/filepath"
 )
+
+// ReadDir reads directory and returns string slice.
+func ReadDir(path string) ([]string, error) {
+	var slice []string
+	err := filepath.Walk(path, func(location string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			slice = append(slice, "dir: "+location)
+		} else {
+			slice = append(slice, "file"+location)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return slice, nil
+}
 
 // CreateDir creates directory to specific path with os.Mkdir.
 func CreateDir(path string) error {
