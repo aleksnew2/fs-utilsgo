bae2cabde81d322372cb9782d51eb14a44fe62de
aleksnew2 <aleks.koyf@gmail.com>
2024-08-17 13:14:26 +0300

    dir.go: create ReadDirQ

--- dir.go
+++ dir.go
@@ -6,7 +6,16 @@ import (
 	"path/filepath"
 )
 
+// Dir is structure where contains information about
+// specific directory.
+// Should be initialized by functions ReadDir and ReadDirQ.
+type Dir struct {
+	Path     string
+	Children []string
+}
+
 // ReadDir reads directory and returns string slice.
+// If there's error, returns nil and error.
 func ReadDir(path string) ([]string, error) {
 	var slice []string
 	err := filepath.Walk(path, func(location string, info fs.FileInfo, err error) error {
@@ -28,6 +37,31 @@ func ReadDir(path string) ([]string, error) {
 	}
 
 	return slice, nil
+}
+
+// ReadDirQ reads directory and returns Dir object.
+// If there's error, returns nil and error.
+func ReadDirQ(path string) (*Dir, error) {
+	var children []string
+	err := filepath.Walk(path, func(location string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			children = append(children, "dir: "+location)
+		} else {
+			children = append(children, "file: "+location)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Dir{path, children}, nil
 }
 
 // CreateDir creates directory to specific path with os.Mkdir.
