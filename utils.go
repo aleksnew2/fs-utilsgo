6ef3b491781b289c8581683b253a2ebcd651a4e8
aleksnew2 <aleks.koyf@gmail.com>
2024-08-18 16:22:16 +0300

    utils.go: add generateID

--- utils.go
+++ utils.go
@@ -1,5 +1,7 @@
 package fs_utils
 
+import "math/rand"
+
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 const (
@@ -7,3 +9,22 @@ const (
 	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
 	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
 )
+
+// Generates random ID with n length.
+func generateID(n int) string {
+	b := make([]byte, n)
+
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
